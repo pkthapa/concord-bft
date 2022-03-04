ec7a982c2c963d788d35968ad1d6bb6165fd7b54
Pankaj <pkumarthapa@vmware.com>
2022-03-04 17:05:04 +0530

    Some EdDSA code added.

--- util/include/openssl_utils.hpp
+++ util/include/openssl_utils.hpp
@@ -16,36 +16,45 @@
 #include <memory>
 #include "crypto_interface.hpp"
 
+// OpenSSL includes.
+#include <openssl/evp.h>
+
+using std::string;
+using std::unique_ptr;
+
 namespace concord::util::openssl_utils {
 enum class KeyFormat : std::uint16_t { HexaDecimalStrippedFormat, PemFormat };
 
 // This class implements OpenSSL's EdDSA signer.
 class EdDSA_Signer : public concord::util::cryptointerface::ISigner {
  public:
-  EdDSA_Signer(const std::string& str_priv_key, KeyFormat fmt);
-  std::string sign(const std::string& data) override;
+  EdDSA_Signer(const string& str_priv_key, KeyFormat fmt);
+  string sign(const string& data) override;
   uint32_t signatureLength() const override;
-  std::string getPrivKey() const override { return key_str_; }
+  string getPrivKey() const override { return key_str_; }
   ~EdDSA_Signer();
 
  private:
   class Impl;
-  std::unique_ptr<Impl> impl_;
-  std::string key_str_;
+  unique_ptr<Impl> impl_;
+  string key_str_;
 };
 
 // This class implements OpenSSL's EdDSA verifier.
 class EdDSA_Verifier : public concord::util::cryptointerface::IVerifier {
  public:
-  EdDSA_Verifier(const std::string& str_pub_key, KeyFormat fmt);
-  bool verify(const std::string& data, const std::string& sig) const override;
+  EdDSA_Verifier(const string& str_pub_key, KeyFormat fmt);
+  bool verify(const string& data, const string& sig) const override;
   uint32_t signatureLength() const override;
-  std::string getPubKey() const override { return key_str_; }
+  string getPubKey() const override {
+    return "";
+    // return key_str_;
+  }
   ~EdDSA_Verifier();
 
  private:
   class Impl;
-  std::unique_ptr<Impl> impl_;
-  std::string key_str_;
+  unique_ptr<Impl> impl_;
+  string key_str_;
 };
 }  // namespace concord::util::openssl_utils
--- util/src/openssl_utils.cpp
+++ util/src/openssl_utils.cpp
@@ -12,14 +12,61 @@
 // file.
 
 #include "openssl_utils.hpp"
+
 namespace concord::util::openssl_utils {
+
+// Deleter classes.
+class EVP_PKEY_CTX_Deleter {
+ public:
+  void operator()(EVP_PKEY_CTX* p) const { EVP_PKEY_CTX_free(p); }
+};
+
+class EVP_MD_CTX_Deleter {
+ public:
+  void operator()(EVP_MD_CTX* p) const { EVP_MD_CTX_free(p); }
+};
+
 class EdDSA_Signer::Impl {};
 
-EdDSA_Signer::EdDSA_Signer(const std::string& str_priv_key, KeyFormat fmt) {}
-std::string EdDSA_Signer::sign(const std::string& data) {}
-uint32_t EdDSA_Signer::signatureLength() const {}
+EdDSA_Signer::EdDSA_Signer(const string& str_priv_key, KeyFormat fmt) {}
 
-EdDSA_Verifier::EdDSA_Verifier(const std::string& str_pub_key, KeyFormat fmt) {}
-bool EdDSA_Verifier::verify(const std::string& data, const std::string& sig) const {}
-uint32_t EdDSA_Verifier::signatureLength() const {}
+string EdDSA_Signer::sign(const string& data) {
+  EVP_PKEY* ed_pkey = nullptr;
+  // EVP_MD_CTX* ed_mdctx = nullptr;
+
+  unique_ptr<EVP_MD_CTX, EVP_MD_CTX_Deleter> ed_mdctx(EVP_MD_CTX_new(), EVP_MD_CTX_Deleter());
+  if (nullptr == ed_mdctx) {
+  }
+
+  unique_ptr<EVP_PKEY_CTX, EVP_PKEY_CTX_Deleter> ed_pctx(EVP_PKEY_CTX_new_id(NID_ED25519, nullptr),
+                                                         EVP_PKEY_CTX_Deleter());
+  if ((nullptr == ed_pctx) || (EVP_PKEY_keygen_init(ed_pctx.get()) <= 0) ||
+      (EVP_PKEY_keygen(ed_pctx.get(), &ed_pkey) <= 0)) {
+    // EVP_PKEY_CTX_free(ed_pctx.get());
+  }
+
+  if (!EVP_DigestSignInit(ed_mdctx.get(), nullptr, nullptr, nullptr, ed_pkey)) {
+    EVP_PKEY_free(ed_pkey);
+  }
+
+  size_t siglen{0}, tbslen{64};
+  string signature(64, 0);
+  if (0 == EVP_DigestSign(ed_mdctx.get(),
+                          reinterpret_cast<unsigned char*>(signature.data()),
+                          &siglen,
+                          reinterpret_cast<const unsigned char*>(data.data()),
+                          tbslen)) {
+  }
+  return signature;
+}
+
+uint32_t EdDSA_Signer::signatureLength() const { return 0; }
+
+class EdDSA_Verifier::Impl {};
+
+EdDSA_Verifier::EdDSA_Verifier(const string& str_pub_key, KeyFormat fmt) {}
+
+bool EdDSA_Verifier::verify(const string& data, const string& sig) const { return true; }
+
+uint32_t EdDSA_Verifier::signatureLength() const { return 0; }
 }  // namespace concord::util::openssl_utils
