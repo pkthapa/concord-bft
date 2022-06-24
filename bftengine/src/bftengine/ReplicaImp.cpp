69a749faba3ef9e94f02d9ffd976a2d41e80e8b3
Toly Kournik <50289004+toly-kournik@users.noreply.github.com>
2022-06-24 11:58:31 +0300

    Reserved Pages divergence after State Transfer fix (#2692)
    1. test_state_transfer_rvt_validity_after_pruning in test_skvbc_state_transfer was disabled because it revealed the Reserved Pages divergence bug.
    Two Reserved Pages clients contributed to this bug:
    - EpochManager, which was setting the epoch on every checkpoint AFTER the checkpointing the state with reserved pages.
       It caused its reserved page to enter the next checkpoint.
       If the current checkpoint was trasferred to another replica, since this replica wouldn't execute requests for the received checkpoint,
       it wouldn't add an epoch to the next state as well and the pages will diverge.
       The solution is to set epoch BEFORE checkpoinntinng.
    - ClientKeyStore:
       when loading from its reserved page, if the page is empty would ALWAYS publish its client keys, even if the publishig is disabled and other replicas were not publishig their client keys.
       The solution is to check whether publishing is enabled when loading from the reserved pages.
    
    2. Re-enable test_state_transfer_rvt_validity_after_pruning.
    3. Add replica ID to the concord.log files in simpleKVBC tests (Apollo): concord-<RID>.log, so they will be split per replica.
       This allows for easier debugging without a need to visit apollo/logs.

--- bftengine/src/bftengine/ReplicaImp.cpp
+++ bftengine/src/bftengine/ReplicaImp.cpp
@@ -5128,6 +5128,11 @@ void ReplicaImp::finishExecutePrePrepareMsg(PrePrepareMsg *ppMsg,
   LOG_INFO(CNSUS, "Finished execution of request seqNum:" << ppMsg->seqNumber());
   uint64_t checkpointNum{};
   if ((lastExecutedSeqNum + 1) % checkpointWindowSize == 0) {
+    // Save the epoch to the reserved pages
+    auto epochMgr = bftEngine::EpochManager::instance();
+    auto epochNum = epochMgr.getSelfEpochNumber();
+    epochMgr.setSelfEpochNumber(epochNum);
+    epochMgr.setGlobalEpochNumber(epochNum);
     checkpointNum = (lastExecutedSeqNum + 1) / checkpointWindowSize;
     stateTransfer->createCheckpointOfCurrentState(
         checkpointNum);  // TODO(GG): should make sure that this operation is idempotent, even if it was partially
@@ -5197,10 +5202,6 @@ void ReplicaImp::finalizeExecution() {
   }
 
   if (lastExecutedSeqNum % checkpointWindowSize == 0) {
-    // Load the epoch to the reserved pages
-    auto epoch = bftEngine::EpochManager::instance().getSelfEpochNumber();
-    bftEngine::EpochManager::instance().setSelfEpochNumber(epoch);
-    bftEngine::EpochManager::instance().setGlobalEpochNumber(epoch);
     Digest stateDigest, reservedPagesDigest, rvbDataDigest;
     CheckpointMsg::State checkState;
     const uint64_t checkpointNum = lastExecutedSeqNum / checkpointWindowSize;
@@ -5479,6 +5480,11 @@ void ReplicaImp::executeRequestsInPrePrepareMsg(concordUtils::SpanWrapper &paren
   }
   uint64_t checkpointNum{};
   if ((lastExecutedSeqNum + 1) % checkpointWindowSize == 0) {
+    // Save the epoch to the reserved pages
+    auto epochMgr = bftEngine::EpochManager::instance();
+    auto epochNum = epochMgr.getSelfEpochNumber();
+    epochMgr.setSelfEpochNumber(epochNum);
+    epochMgr.setGlobalEpochNumber(epochNum);
     checkpointNum = (lastExecutedSeqNum + 1) / checkpointWindowSize;
     stateTransfer->createCheckpointOfCurrentState(checkpointNum);
     checkpoint_times_.start(lastExecutedSeqNum);
@@ -5521,10 +5527,6 @@ void ReplicaImp::executeRequestsInPrePrepareMsg(concordUtils::SpanWrapper &paren
   }
 
   if (lastExecutedSeqNum % checkpointWindowSize == 0) {
-    // Load the epoch to the reserved pages
-    auto epoch = bftEngine::EpochManager::instance().getSelfEpochNumber();
-    bftEngine::EpochManager::instance().setSelfEpochNumber(epoch);
-    bftEngine::EpochManager::instance().setGlobalEpochNumber(epoch);
     Digest stateDigest, reservedPagesDigest, rvbDataDigest;
     CheckpointMsg::State state;
     const uint64_t checkpointNum = lastExecutedSeqNum / checkpointWindowSize;
